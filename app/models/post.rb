1c9feedf391fd7b78e80f6d83742b699e2b6b9f4
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-05-06 11:41:08 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: {in: %w(Fiction Non-fiction)}
-  validate 
+  validate :non_clickbait
   
   def non_clickbait
     errors.add(:title, "cannot contain the characters 
